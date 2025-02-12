1ce0baebc9813ddb9bf3fd5a137b77650777c83c
Jesus-sunuc <mariano.code4guate@gmail.com>
2025-02-11 20:51:25 -0700

    Add gRPC service to BadgerClan project

---
+++ BadgerClan.GrpcBotApi/Services/BotStrategyService.cs
@@ -0,0 +1,42 @@
+using Grpc.Core;
+using BadgerClan.GrpcBotApi;
+using BadgerClan.Logic.Bot;
+
+namespace BadgerClan.GrpcBotApi.Services;
+
+public class BotStrategyService : BotStrategy.BotStrategyBase
+{
+    private static IBot _currentBot = new NothingBot();
+
+    public override Task<SetStrategyReply> SetStrategy(SetStrategyRequest request, ServerCallContext context)
+    {
+        switch (request.StrategyName.ToLowerInvariant())
+        {
+            case "aggressive":
+                _currentBot = new AggressiveBot();
+                break;
+            case "defensive":
+                _currentBot = new DefensiveBot();
+                break;
+            case "random":
+                _currentBot = new RandomBot();
+                break;
+            default:
+                _currentBot = new NothingBot();
+                break;
+        }
+
+        return Task.FromResult(new SetStrategyReply
+        {
+            Message = $"Strategy changed to {request.StrategyName}"
+        });
+    }
+
+    public override Task<GetCurrentStrategyReply> GetCurrentStrategy(GetCurrentStrategyRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new GetCurrentStrategyReply
+        {
+            CurrentStrategy = _currentBot.GetType().Name
+        });
+    }
+}
